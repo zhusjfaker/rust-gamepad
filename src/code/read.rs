5622d1403f0921363be05ca8a309b17c17645651
zhushijie.jayson <zhushijie.jayson@bytedance.com>
2022-10-01 21:41:16 +0800

    fix: bug

--- src/code/read.rs
+++ src/code/read.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 use crate::code::fsfile::{path_resolve, safe_open_file, WriteLine};
 use hidapi::HidDevice;
 
@@ -6,13 +8,13 @@ pub fn gamepad_reading(device: HidDevice) {
     println!("starting to read handle byte data...");
     loop {
         let mut buf = [0u8; 8];
-        let _res = device.read_timeout(&mut buf[..], 1000).unwrap();
-        // println!("Read: {:?}", &buf[..res]);
-        let content = serde_json::to_string(&buf[0..6]).unwrap();
+        let res = device.read_timeout(&mut buf[..], 1000).unwrap();
+        let content = serde_json::to_string(&buf[0..res]).unwrap();
         if content != old_content {
             let record_txt_file = path_resolve("record.txt".to_string());
             let mut file = safe_open_file(record_txt_file).unwrap();
             file.writeline(content.as_str());
+            file.flush().unwrap();
             old_content = content;
         }
     }
